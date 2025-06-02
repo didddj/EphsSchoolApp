c29068409683dc058218599d8baae7d28b321e97
didddj <dharani.akurati@gmail.com>
2025-06-02 10:44:17 -0500

    Initial Commit

---
+++ EphsSchoolApp/EphsSchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EphsSchoolAppApp.swift
+//  EphsSchoolApp
+//
+//  Created by Dharani Akurati on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct EphsSchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EphsSchoolAppTests/EphsSchoolAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EphsSchoolAppTests.swift
+//  EphsSchoolAppTests
+//
+//  Created by Dharani Akurati on 5/30/25.
+//
+
+import Testing
+@testable import EphsSchoolApp
+
+struct EphsSchoolAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
